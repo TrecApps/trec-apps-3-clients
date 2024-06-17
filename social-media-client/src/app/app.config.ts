382173aa684b3a1a72f80ad6adc38da5705a6ecc
John Jacko <jljacko@outlook.com>
2024-06-16 19:41:31 -0500

    Added material to project

--- social-media-client/src/app/app.config.ts
+++ social-media-client/src/app/app.config.ts
@@ -6,7 +6,8 @@ import { } from '@angular/forms';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch()), provideAnimations()]
+  providers: [provideRouter(routes), provideHttpClient(withFetch()), provideAnimations(), provideAnimationsAsync()]
 };

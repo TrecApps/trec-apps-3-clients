c83b1e595b59af1da844731f63acca3843f6ed03
John Jacko <jljacko@outlook.com>
2024-06-30 21:48:56 -0500

    Corrected Routing for not found profiles

--- social-media-client/src/app/services/user.service.ts
+++ social-media-client/src/app/services/user.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { TcBrand, TcUser, UserInfo } from '../models/UserObjs';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { environment } from '../environments/environment';
+import { ProfileDetailsService } from './profile-details.service';
 
 
 @Injectable({
@@ -57,7 +58,7 @@ export class UserService {
     return "assets/icons/non-profile.png";
   }
 
-  constructor(private authService: AuthService, private router: Router, private client: HttpClient) { }
+  constructor(private authService: AuthService, private router: Router, private client: HttpClient, private profService: ProfileDetailsService) { }
 
   defaultPic: string = "assets/icons/non-profile.png";
   profilePic: string = this.defaultPic;
@@ -72,15 +73,31 @@ export class UserService {
         this.tcUser = response.user;
         this.tcBrand = response.brand;
 
+        let profile = "";
+
         if(this.tcBrand?.id){
           this.profilePic = `${environment.image_service_url}Profile/byBrand/${this.tcBrand.id}?app=${environment.app_name}`
+          profile = `Brand-${this.tcBrand.id}`;
         } else if(this.tcUser?.id){
           this.profilePic = `${environment.image_service_url}Profile/of/${this.tcUser.id}?app=${environment.app_name}`
+          profile = `User-${this.tcUser.id}`;
         } else {
           this.onProfileNotAvailable();
         }
+        if(profile.length){
+          this.profService.getBasicProfile(profile).subscribe({
+            next: () => {
+              callable(true)
+            },
+            error: (response: HttpResponse<any>) =>{
+              this.router.navigateByUrl("/profile")
+            }
+          })
+        } else {
+          callable(true);
+        }
 
-        callable(true);
+        
 
       },
       error: (error: Response | any) => { 
@@ -92,6 +109,8 @@ export class UserService {
         }
       }
     };
+
+    
 
     this.client.get<UserInfo>(`${environment.user_service_url}Auth/User`,{headers: this.authService.getHttpHeaders(true, false)}).subscribe(observe);
 
